257e2137555a292f803596f71b4d307e85d51f6d
zermoser <kittipoj.na@gmail.com>
2024-06-18 13:20:51 +0700

    Add Home2

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
+import Home2 from './pages/HomeJson';
 import Redirect404 from './pages/Redirect404';
 
 export const router = createBrowserRouter([
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
   {
     path: "/home",
     element: <Home />,
+  },
+  {
+    path: "/home2",
+    element: <Home2 />,
   },
   {
     path: "*",
